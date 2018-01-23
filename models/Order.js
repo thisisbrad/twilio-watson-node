f17ee4d9ff803a106bdfe058545dcae26cfc3e70
Brad Beltowski <bradbeltowski@gmail.com>
2018-01-23 01:06:16 -0500

    Fixed Mongoose schema

--- models/Order.js
+++ models/Order.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const OrderSchema = mongoose.Schema(
   {
-    convo_id: { type: Number, required: true, index: { unique: true } },
+    convo_id: { type: String, required: true, index: { unique: true } },
     from: { type: String, required: true, index: true },
     size: { type: String, required: true, index: false },
     flavor: { type: String, required: true, index: false },
