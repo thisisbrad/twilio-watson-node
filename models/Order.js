3e684f75ddd0847ab9d47d296c1fa2f3b86ed4ca
Brad Beltowski <bradbeltowski@gmail.com>
2018-01-23 16:27:08 -0500

    More TODO notes

--- models/Order.js
+++ models/Order.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const OrderSchema = mongoose.Schema(
   {
     convo_id: { type: String, required: true, index: { unique: true } },
-    from: { type: String, required: true, index: true },
+    from: { type: String, required: true, index: true }, // Needs to be Number(7)
     size: { type: String, required: true, index: false },
     flavor: { type: String, required: true, index: false },
-    nuts: { type: String, required: true, index: false },
-    cherry: { type: String, required: true, index: false }
+    nuts: { type: String, required: true, index: false }, // Needs to be bool
+    cherry: { type: String, required: true, index: false } // Needs to be bool
   },
   { timestamps: true }
 );
