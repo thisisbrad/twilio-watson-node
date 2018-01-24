5c316970ed1fe4e4c5b75ed39d67f7197aecb95a
Brad Beltowski <bradbeltowski@gmail.com>
2018-01-24 18:00:46 -0500

    Saving Error logs 😎

--- server/config/index.js
+++ server/config/index.js
@@ -1,10 +1,23 @@
 const bunyan = require('bunyan');
+const error = require('../bin/tmp/error.log');
 
 const appname = 'Waston';
 
 module.exports = {
   applicationName: appname,
-  logger: bunyan.createLogger({ name: appname }),
+  logger: bunyan.createLogger({
+    name: appname,
+    streams: [
+      {
+        level: 'info',
+        stream: process.stdout // log INFO and above to stdout
+      },
+      {
+        level: 'error',
+        path: './server/bin/tmp/error.log' // log ERROR and above to a file
+      }
+    ]
+  }),
   mongodb: {
     uri: 'mongodb://watson:password@ds111618.mlab.com:11618/watson'
   }
