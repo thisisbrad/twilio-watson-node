fe3c8ab1ac6938f80537db3e32f84e81c5dc4064
Brad Beltowski <bradbeltowski@gmail.com>
2018-01-23 22:58:20 -0500

    Lets see now!

--- server/services/watsonService.js
+++ server/services/watsonService.js
@@ -1,6 +1,13 @@
+const ConversationV1 = require('watson-developer-cloud/conversation/v1');
 const config = require('../config');
 
 const log = config.logger;
+// Setup the Watson Converstion with your keys. Use .env file, duh!
+const conversation = new ConversationV1({
+  username: process.env.WATSON_KEY,
+  password: process.env.WATSON_SECRET,
+  version_date: ConversationV1.VERSION_DATE_2016_09_20
+});
 
 const contexts = [];
 // const order = {}; // Setup empty order
@@ -13,7 +20,6 @@ async function sendMessage(message, number, twilioNumber) {
   let index = 0;
   let contextIndex = 0;
   contexts.forEach(value => {
-    // console.log(value.from);
     if (value.from === number) {
       log.info('CONTEXT: ', context);
       context = value.context;
@@ -21,6 +27,9 @@ async function sendMessage(message, number, twilioNumber) {
     }
     index += 1;
   });
+
+  log.info(JSON.stringify(context));
+  log.info(contexts.length);
 
   log.info('DEBUG!', message, number, twilioNumber);
 }
