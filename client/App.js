e6a0b265909c7e12d9673a6bc25dde6417e4f929
Brad Beltowski <bradbeltowski@gmail.com>
2018-01-29 19:08:14 -0500

    Working on Redux in React Native

--- client/App.js
+++ client/App.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StatusBar, StyleSheet } from 'react-native';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import { Provider } from 'react-redux';
+import { configureStore } from './src/store';
+const { persistor, store } = configureStore();
 
-export default class App extends React.Component {
+import Base from './src/components/Base';
+import Loading from './src/components/Loading';
+
+class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={store}>
+        <PersistGate persistor={persistor} loading={<Loading />}>
+          <Base />
+        </PersistGate>
+      </Provider>
     );
   }
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
---
+++ client/src/reducers/orderReducer.js
@@ -0,0 +1,19 @@
+import uuid from 'uuid';
+
+export default function todoReducer(state = [], action) {
+  // console.log('### reducers ###', state, action);
+  switch (action.type) {
+    case 'LOAD_TODOS':
+      return action.todos;
+    case 'ADD_TODO':
+      return [...state, action.newTodo];
+    case 'UPDATE_TODO':
+      const { index, todo } = action;
+      console.log('UPDATE REDUCER ', index, todo);
+      return [...state.slice(0, index), todo, ...state.slice(index + 1)];
+    case 'REMOVE_TODO':
+      return state.filter(todo => (todo._id === action.id ? false : true));
+    default:
+      return state;
+  }
+}
